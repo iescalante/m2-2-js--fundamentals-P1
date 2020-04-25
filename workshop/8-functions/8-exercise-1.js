d5ccbc9d35d6e65c8028aadbc2c34a6879338a79
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-04-25 16:18:33 -0400

    completed 8-2, corrected 8-1

--- workshop/8-functions/8-exercise-1.js
+++ workshop/8-functions/8-exercise-1.js
@@ -6,12 +6,13 @@
 
 function max(num1, num2) {
   if (num1 > num2) {
-    return num1;
+    console.log(num1);
   } else {
-    return num2;
+    console.log(num2);
   }
 }
 console.log(max(num1, num2));
 
 // Call the function
 max(12, 43);
+console.log(max(12, 43));
--- workshop/8-functions/8-exercise-2.js
+++ workshop/8-functions/8-exercise-2.js
@@ -2,16 +2,31 @@
 // Write a function that accepts two integers and *returns*
 // the greater one. (this is slightly different from the previous question.)
 
+function maxValue(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
+
 // Also, wouldn't it be nice if there were a Math method that could simplify the code....
 // https://www.w3schools.com/jsreF/jsref_obj_math.asp
 
 // Q2b
 // What if we wanted to figure out the max of 4 integers, instead of 2?
 
+function maxValue(num1, num2, num3, num4) {
+  return Math.max(num1, num2, num3, num4);
+}
 // STRETCH
 // What if we wanted to write a function which took an unlimited number of
 // integers, and returned the largest one?
 
+function maxValue(...numX) {
+  return Math.max(...numX);
+}
+console.log(maxValue());
 // All of the following calls should be valid:
 maxValue(4, 2); // 4
 maxValue(4, 2, 8, 1, 5, 10); // 10
