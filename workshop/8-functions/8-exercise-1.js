447c63d08f72f30f17b59c72eecb5bcdf08f06ee
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-04-25 15:13:28 -0400

    completed 8-1

--- workshop/8-functions/8-exercise-1.js
+++ workshop/8-functions/8-exercise-1.js
@@ -3,7 +3,15 @@
 // the greater one, using `console.log`
 
 // Define the function
-function max(num1, num2) {}
+
+function max(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
+console.log(max(num1, num2));
 
 // Call the function
 max(12, 43);
