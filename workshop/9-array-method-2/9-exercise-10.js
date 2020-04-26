2067a0c9d6a75d3028b2c34fb4fe63351ea153d3
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-04-26 16:01:03 -0400

    completed 9-10

--- workshop/9-array-method-2/9-exercise-10.js
+++ workshop/9-array-method-2/9-exercise-10.js
@@ -9,11 +9,30 @@
 // <100 is A
 
 // Write function here
+let grades = [];
+function calculateAverage(grades) {
+  let sum = grades.reduce(function (a, b) {
+    return a + b;
+  }, 0);
 
+  if (Math.round(sum / grades.length) < 60) {
+    return "F";
+  }
+  if (Math.round(sum / grades.length) < 70) {
+    return "D";
+  }
+  if (Math.round(sum / grades.length) < 80) {
+    return "C";
+  }
+  if (Math.round(sum / grades.length) < 90) {
+    return "B";
+  }
+  if (Math.round(sum / grades.length) < 100) {
+    return "A";
+  }
+}
+console.log(calculateAverage([48, 95, 65, 48, 59, 78, 72, 65]));
 
- 
 // Call the function with [48, 95, 65, 48, 59, 78, 72, 65]
 
 // Try with other values as well
-
-
