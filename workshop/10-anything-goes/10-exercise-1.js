92c3e3c2814873d8bb7dc64f1db6a7123c835982
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-04-26 16:30:40 -0400

    completed 10-1

--- workshop/10-anything-goes/10-exercise-1.js
+++ workshop/10-anything-goes/10-exercise-1.js
@@ -4,14 +4,13 @@
 // The first character has position 0, which causes the last one to be found at position string.length - 1.
 // In other words, a two-character string has length 2, and its characters have positions 0 and 1.
 
-
 // Q1
 // Write a function countBs that takes a string as its only argument
 // and returns a number that indicates how many uppercase “B”
 // characters there are in the string.
-
+let str = "";
 function countBs(str) {
-
+  return str.split("B").length - 1;
 }
 // -------------------------------------------------------------------------
-console.log('Q1: ', countBs('BananaBabyBubbles'));
+console.log(countBs("BananaBabyBubbles"));
