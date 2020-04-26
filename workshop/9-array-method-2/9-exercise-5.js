ab1619b07ebadcdff44077650a21c2b7157d1f59
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-04-26 14:04:33 -0400

    completed 9-5

--- workshop/9-array-method-2/9-exercise-5.js
+++ workshop/9-array-method-2/9-exercise-5.js
@@ -11,11 +11,11 @@
 // Q5
 // Returns a new list with all the elements of lst that are length
 // greater than 5
-function keepLong(lst) {
-  // lst is an array of strings
-}
+// lst is an array of strings
 
-console.log(
-  'Q5 keepLong()',
-  keepLong(['Cyborg', 'Robin', 'Batman', 'Superman', 'Aquaman', 'Flash'])
-);
+let keepLong = ["Cyborg", "Robin", "Batman", "Superman", "Aquaman", "Flash"];
+let onlyLong = keepLong.filter(function (item) {
+  return item.length > 5;
+});
+
+console.log(onlyLong);
