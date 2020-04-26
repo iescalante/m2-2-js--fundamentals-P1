52681a5f77a3d93e52e960ebcc5a90edf3b01b21
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-04-26 00:21:35 -0400

    completed 9-4

--- workshop/9-array-method-2/9-exercise-4.js
+++ workshop/9-array-method-2/9-exercise-4.js
@@ -11,12 +11,11 @@
 // Q4
 // Write a function that accepts a list of numbers and returns a new list with
 // all of the even numbers removed.
-function removeEvens(lst) {
-  // lst is an array of strings
-}
+let removeEvens = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+let noEvens = removeEvens.filter(function (number) {
+  return number % 2 === 1;
+});
 
-console.log(
-  'Q4 removeEvens()',
-  removeEvens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
-);
+console.log(noEvens);
+
 // should log [1, 3, 5, 7, 9, 11]
