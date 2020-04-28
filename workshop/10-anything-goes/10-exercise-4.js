adedf204084e04abe200ea9d5ece571a50514926
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-04-27 20:23:30 -0400

    completed 10-4

--- workshop/10-anything-goes/10-exercise-4.js
+++ workshop/10-anything-goes/10-exercise-4.js
@@ -3,9 +3,11 @@
 // This function should accept an array as an argument and return a new array.
 
 // Hint: consider using .filter(), .indexOf() and .lastIndexOf()
-
+let array = [];
 function filterNonUnique(array) {
-
+  return array.filter(function (a, b) {
+    return array.indexOf(a) === b;
+  });
 }
 
-console.log(filterNonUnique([1,2,3,3,4,5,6,7,7,8]));
+console.log(filterNonUnique([1, 2, 3, 3, 4, 5, 6, 7, 7, 8]));
